9b5bd7dd074fdb01fb39751c74f9e789a153560b
srwight <srwight@gmail.com>
2020-04-03 19:48:03 -0400

    move get_r_prism and rotate_prism to arrays.py

--- utils/arrays.py
+++ utils/arrays.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import randint, random
 
 def normalize_array(array_in:np.array) -> np.array:
 
@@ -8,3 +9,35 @@ def normalize_array(array_in:np.array) -> np.array:
     normalized_array = (array_in - array_min) / array_range
 
     return normalized_array
+
+def get_r_prism(source:np.array, zyx_sides:tuple, zyx_corner:tuple) -> np.array:
+    if any((side + corner) > shape for side, corner, shape in zip(zyx_sides, zyx_corner, source.shape)):
+        raise IndexError("Input array is not big enough to collect sample.")
+
+    cube = {
+        "z_start":zyx_corner[0],
+        "z_end":zyx_corner[0] + zyx_sides[0],
+        "y_start":zyx_corner[1],
+        "y_end":zyx_corner[1] + zyx_sides[1],
+        "x_start":zyx_corner[2],
+        "x_end":zyx_corner[2] + zyx_sides[2]
+    }
+
+    ret_cube = source[
+        cube['z_start']:cube['z_end'],
+        cube['y_start']:cube['y_end'],
+        cube['x_start']:cube['x_end']
+    ]
+
+    return ret_cube
+
+def rotate_prism(arr_in:np.array, rotate:float, axes:list) -> np.array:
+    # Rotate by some fraction of rotate degrees
+    out_array = None
+    for axis in axes:
+        degrees = rotate * random() * (randint(0,1) * 2 - 1)
+        if axis not in (0,1,2):
+            raise IndexError('Invalid Axis')
+        rotation_axes = tuple(a for a in [0,1,2] if a != axis )
+        out_array = ndimage.rotate(arr_in, degrees, rotation_axes, reshape=False)
+    return out_array
--- utils/get_data.py
+++ utils/get_data.py
@@ -7,9 +7,9 @@ from scipy import ndimage
 import numpy as np, pandas as pd
 import os, warnings
 from random import randint, random
-from utils.get_dicom import generate_series_from_directories
+from .get_dicom import generate_series_from_directories
 from .decorators import infinite_loop, nested_dirs, infinite_looper
-from .arrays import normalize_array
+from .arrays import normalize_array, get_r_prism, rotate_prism
 
 def get_vox_coords(mm_coords:tuple, mm_per_vox:tuple, mm_origin:tuple, padding:int=0) -> tuple:
     vox_coords = tuple(
@@ -25,38 +25,6 @@ def get_vox_coords(mm_coords:tuple, mm_per_vox:tuple, mm_origin:tuple, padding:i
         )
     )
     return vox_coords
-
-def get_r_prism(source:np.array, zyx_sides:tuple, zyx_corner:tuple) -> np.array:
-    if any((side + corner) > shape for side, corner, shape in zip(zyx_sides, zyx_corner, source.shape)):
-        raise IndexError("Input array is not big enough to collect sample.")
-
-    cube = {
-        "z_start":zyx_corner[0],
-        "z_end":zyx_corner[0] + zyx_sides[0],
-        "y_start":zyx_corner[1],
-        "y_end":zyx_corner[1] + zyx_sides[1],
-        "x_start":zyx_corner[2],
-        "x_end":zyx_corner[2] + zyx_sides[2]
-    }
-
-    ret_cube = source[
-        cube['z_start']:cube['z_end'],
-        cube['y_start']:cube['y_end'],
-        cube['x_start']:cube['x_end']
-    ]
-
-    return ret_cube
-
-def rotate_prism(arr_in:np.array, rotate:float, axes:list) -> np.array:
-    # Rotate by some fraction of rotate degrees
-    out_array = None
-    for axis in axes:
-        degrees = rotate * random() * (randint(0,1) * 2 - 1)
-        if axis not in (0,1,2):
-            raise IndexError('Invalid Axis')
-        rotation_axes = tuple(a for a in [0,1,2] if a != axis )
-        out_array = ndimage.rotate(arr_in, degrees, rotation_axes, reshape=False)
-    return out_array
 
 def scan_from_file(filename:str):
     image = ReadImage(filename)
